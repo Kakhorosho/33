0f7adb9321a1515a7df5db7ac57c7f715d24375e
Kakhorosho <159240210+Kakhorosho@users.noreply.github.com>
2024-02-09 16:31:14 +0700

    Add files via upload

---
+++ paint.py
@@ -0,0 +1,242 @@
+t.fillcolor('hotpink')
+t.begin_fill()
+t.rt(90)
+triangle(75, -75, 106.06602)
+t.end_fill()
+
+t.fillcolor('cyan')
+t.begin_fill()
+t.rt(135)
+triangle(0, -150, 150)
+t.end_fill()
+
+t.begin_fill()
+square(0, 0, 106.06602)
+t.end_fill()
+
+t.begin_fill()
+t.rt(90)
+prl(-150, -150, 106.06602, 150)
+t.end_fill()
+
+t.rt(90)
+
+# An algorithm that draws a tangram in the shape of a rocket.
+t.fillcolor('purple')
+t.begin_fill()
+t.rt(-135)
+triangle(337.5, -187.5, 53.03301)
+t.end_fill()
+
+t.fillcolor('cyan')
+t.begin_fill()
+t.rt(45)
+triangle(337.5, -262.5, 75)
+t.end_fill()
+
+t.fillcolor('yellow')
+t.begin_fill()
+t.rt(45)
+triangle(262.5, -337.5, 106.06602)
+t.end_fill()
+
+t.fillcolor('red')
+t.begin_fill()
+t.rt(180)
+triangle(337.5, -262.5, 106.06602)
+t.end_fill()
+
+t.begin_fill()
+t.rt(-45)
+square(262.5, -337.5, 53.03301)
+t.end_fill()
+
+t.fillcolor('hotpink')
+t.begin_fill()
+t.rt(45)
+triangle(225, -450, 53.03301)
+t.end_fill()
+
+t.begin_fill()
+t.rt(45)
+prl(337.5, -375, 53.03301, 75)
+t.end_fill()
+
+# An algorithm that draws a tangram in the shape of a boat.
+t.fillcolor('red')
+t.begin_fill()
+t.rt(-45)
+triangle(-300, -375, 106.06602)
+t.end_fill()
+
+t.begin_fill()
+t.rt(135)
+square(-225, -300, 53.03301)
+t.end_fill()
+
+t.fillcolor('hotpink')
+t.begin_fill()
+t.rt(-45)
+triangle(-225, -375, 53.03301)
+t.end_fill()
+
+t.fillcolor('cyan')
+t.begin_fill()
+t.rt(-180)
+triangle(-331.06602, -375, 75)
+t.end_fill()
+
+t.begin_fill()
+t.rt(-90)
+prl(-384.09903, -375, 53.03301, 75)
+t.end_fill()
+
+t.fillcolor('yellow')
+t.begin_fill()
+t.rt(45)
+triangle(-406.06602, -365, 106.06602)
+t.end_fill()
+
+t.fillcolor('purple')
+t.begin_fill()
+t.rt(90)
+triangle(-300, -171.96699, 53.03301)
+t.end_fill()
+
+t.rt(-90)
+
+# An algorithm that draws a human-shaped tangram.
+t.fillcolor('yellow')
+t.begin_fill()
+t.rt(-180)
+triangle(-193.93398, 70.56602, 106.06602)
+t.end_fill()
+
+t.fillcolor('red')
+t.begin_fill()
+t.rt(-270)
+triangle(-300, -34.5, 106.06602)
+t.end_fill()
+
+t.fillcolor('cyan')
+t.begin_fill()
+t.rt(-180)
+triangle(-300, -34.5, 75)
+t.end_fill()
+triangle(-300, 262.5, 53.03301)
+t.end_fill()
+
+t.fillcolor('cyan')
+t.begin_fill()
+t.rt(45)
+triangle(-406.06602, 193.93398, 75)
+t.end_fill()
+
+t.fillcolor('purple')
+t.begin_fill()
+t.rt(90)
+triangle(-331.06602, 246.96699, 53.03301)
+t.end_fill()
+
+t.fillcolor('red')
+t.begin_fill()
+t.rt(-90)
+triangle(-406.06602, 300, 106.06602)
+t.end_fill()
+
+t.begin_fill()
+t.rt(-45)
+square(-300, 379.549515, 53.03301)
+t.end_fill()
+
+t.begin_fill()
+t.left(180)
+prl(-379.549515, 485.615535, 53.03301, 75)
+t.end_fill()
+
+t.rt(45)
+
+# An algorithm that draws a cat-shaped tangram.
+t.fillcolor('red')
+t.begin_fill()
+triangle(193.93398, 193.93398, 106.06602)
+t.end_fill()
+
+t.begin_fill()
+t.rt(-90)
+prl(300, 193.93398, 53.03301, 75)
+t.end_fill()
+
+t.fillcolor('yellow')
+t.begin_fill()
+t.rt(45)
+triangle(225, 225, 106.06602)
+t.end_fill()
+
+t.fillcolor('cyan')
+t.begin_fill()
+t.rt(-180)
+triangle(225, 375, 75)
+t.end_fill()
+
+t.begin_fill()
+t.rt(-45)
+square(225, 450, 53.03301)
+t.end_fill()
+
+t.fillcolor('hotpink')
+t.begin_fill()
+t.rt(-135)
+triangle(262.5, 487.5, 53.03301)
+t.end_fill()
+
+t.fillcolor('purple')
+t.begin_fill()
+t.rt(180)
+triangle(187.5, 412.5, 53.03301)
+t.end_fill()
+
+t.rt(45)
+
+# An algorithm that draws a tangram in the shape of a ballerina.
+t.begin_fill()
+t.rt(90)
+square(300, 150, 53.03301)
+t.end_fill()
+
+t.fillcolor('yellow')
+t.begin_fill()
+t.rt(-270)
+triangle(225, -106.06602, 106.06602)
+t.end_fill()
+
+t.fillcolor('red')
+t.begin_fill()
+t.rt(-180)
+triangle(406.06602, 75, 106.06602)
+t.end_fill()
+
+t.begin_fill()
+t.rt(90)
+prl(350, 20, 53.03301, 75)
+t.end_fill()
+
+t.fillcolor('cyan')
+t.begin_fill()
+t.rt(45)
+triangle(330, -130, 75)
+t.end_fill()
+
+t.fillcolor('hotpink')
+t.begin_fill()
+t.rt(-135)
+triangle(244, -86, 53.03301)
+t.end_fill()
+
+t.fillcolor('purple')
+t.begin_fill()
+t.rt(180)
+triangle(309, -150, 53.03301)
+t.end_fill()
+
+window.mainloop()
